5c95edfb1c4b7bee1c62432c40969ed68ac68b76
Madhav-Gupta-28 <guptamadhav2828@gmail.com>
2023-10-23 16:02:21 +0530

    footer done

--- app/app/page.js
+++ app/app/page.js
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
 
 export default function Home() {
   return (
     <ChakraProvider>
       <Navbar/>
+      <Footer/>
     </ChakraProvider>
  
   )
--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,10 +1,91 @@
 import React from 'react'
 
+// INTERNAL IMPORT 
+import Image from 'next/image';
 import Style from "./Footer.module.css";
+import images from  "../../img";
+import HelpCenter from '../Navbar/HelpCenter/HelpCenter';
+import Discover from '../Navbar/Discover/Discover';
+
+// ICONS IMPORT 
+import { GrClose } from 'react-icons/gr';
+import {TiSocialFacebook, TiSocialLinkedin , TiSocialTwitter  , TiSocialYoutube , TiSocialInstagram, 
+  TiArrowSortedDown
+, TiArrowSortedUp
+} from "react-icons/ti";
+
+import {RiSendPlaneFill} from "react-icons/ri"
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={Style.FOOTER}>
+
+      <div className={Style.FOOTER_BOX}>
+        <div className={Style.FOOTER_BOX_SOCIAL}>
+          <Image src={images.logo} alt='Footer Logo' height={100} width={100} />
+
+          <p>The World's First and Largest digital marketplace for collectibiles and non-fungible 
+            tokens (NFTs). But and Sell exclusive digital items.
+          </p>
+
+          <div className={Style.FOOTER_SOCIAL}>
+
+            <a href='#' >
+              <TiSocialInstagram/>
+            </a>
+            <a href='#' >
+              <TiSocialLinkedin/>
+            </a>
+            <a href='#' >
+              <TiSocialFacebook/>
+            </a>
+            <a href='#' >
+              <TiSocialTwitter/>
+            </a>
+            <a href='#' >
+              <TiSocialYoutube/>
+            </a>
+
+
+          </div>
+
+        </div>
+
+
+
+      {/* DISCOVER COMPONENT */}
+        <div className={Style.FOOTER_BOX_DISCOVER_MENU}>
+          <h>Discover</h>
+          <Discover/>
+        </div>
+
+
+      {/* HELPCENTER COMPONENT */}
+      <div className={Style.FOOTER_BOX_HELPCENTER_MENU}>
+          <h>HELP CENTER</h>
+          <HelpCenter/>
+        </div>
+
+      {/* SUBSCRIBE COMPONENT */}
+        <div  className={Style.SUBSCRIBE}>
+          <h3>Subscribe</h3>
+    
+      <div className={Style.SUBSCRIBE_BOX}>
+        <input type='email' placeholder='Type Your Email *' />
+        <RiSendPlaneFill className={Style.SEND_ICON} />
+      </div>
+
+      <div className={Style.SUBSCRIBE_BOX_INFO}>
+      The World's First and Largest digital marketplace for collectibiles and non-fungible 
+            tokens (NFTs). But and Sell exclusive digital items.
+      </div>
+      </div>
+
+      </div>
+
+
+    </div>
   )
 }
 
